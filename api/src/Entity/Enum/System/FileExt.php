3797118f9361af999db444c58efb3ac883ad968c
Matheus S. <matheussilvestre3@gmail.com>
2025-05-29 06:42:00 -0300

    Adiciona novas extensões de arquivos

--- api/src/Entity/Enum/System/FileExt.php
+++ api/src/Entity/Enum/System/FileExt.php
@@ -9,6 +9,10 @@ enum FileExt: string {
     case PDF = 'pdf';
     case XLSX = 'xlsx';
     case TXT = 'txt';
+    case JPEG = 'jpeg';
+    case PNG = 'png';
+    case WEBP = 'webp';
+    case MP4 = 'mp4';
     case PCAP = 'pcap';
     case PCAPNG = 'pcapng';
 
