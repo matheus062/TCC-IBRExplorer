6daa9559fa2b0f360f167781a256f6bd4ad8195f
Matheus S. <matheussilvestre3@gmail.com>
2025-05-29 06:41:44 -0300

    Ajusta tipos de usuário e estrutura do banco inicial

--- api/src/Entity/Enum/User/UserRoleType.php
+++ api/src/Entity/Enum/User/UserRoleType.php
@@ -8,15 +8,7 @@ enum UserRoleType: int {
 
     case System = 1;
     case Admin = 2;
-    case Management = 3;
-    case Support = 4;
-    case Logistics = 5;
-    case Operational = 6;
-    case Inspector = 7;
-    case Documentation = 8;
-    case Reviewer = 9;
-    case Certifier = 10;
-    case Finance = 11;
-    case Customer = 12;
+    case Support = 3;
+    case User = 4;
 
 }
