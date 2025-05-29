b490da48ba4f0643fd288130475133bb152a41af
Matheus S. <matheussilvestre3@gmail.com>
2025-05-29 06:41:28 -0300

    Adiciona novos ContentType

--- api/src/Api/Enum/ContentType.php
+++ api/src/Api/Enum/ContentType.php
@@ -5,5 +5,14 @@ namespace IBRExplorer\Api\Enum;
 enum ContentType: string {
 
     case Json = 'application/json';
+    case Pdf = 'application/pdf';
+    case Xlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    case Txt = 'text/plain';
+    case Jpeg = 'image/jpeg';
+    case Png = 'image/png';
+    case Webp = 'image/webp';
+    case Mp4 = 'video/mp4';
+    case Pcap = 'application/vnd.tcpdump.pcap';
+    case Pcapng = 'application/octet-stream';
 
 }
